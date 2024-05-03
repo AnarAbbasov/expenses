9f53401816096ad83fb69fa5096bb0965c914aa8
Anar <anar@rocky.anar.com>
2024-05-02 20:29:41 -0700

    initial commit

---
+++ test.c
@@ -0,0 +1,40 @@
+#include <mysql/mysql.h>
+#include <stdio.h>
+
+int main() {
+    MYSQL *conn;
+    MYSQL_RES *res;
+    MYSQL_ROW row;
+
+    char *server = "192.168.1.110";
+    char *user = "root";
+    char *password = "password"; // Replace with your actual password
+    char *database = "expenses"; // Replace with your desired database name
+
+    conn = mysql_init(NULL);
+
+    // Connect to the database
+    if (!mysql_real_connect(conn, server, user, password, database, 0, NULL, 0)) {
+        fprintf(stderr, "Error: %s\n", mysql_error(conn));
+        exit(1);
+    }
+
+    // Execute an SQL query
+    if (mysql_query(conn, "SHOW TABLES")) {
+        fprintf(stderr, "Error: %s\n", mysql_error(conn));
+        exit(1);
+    }
+
+    // Retrieve and print table names
+    res = mysql_use_result(conn);
+    printf("MySQL Tables in %s database:\n", database);
+    while ((row = mysql_fetch_row(res)) != NULL) {
+        printf("%s\n", row[0]);
+    }
+
+    // Clean up
+    mysql_free_result(res);
+    mysql_close(conn);
+
+    return 0;
+}
