f3a19d438639353a7f6842db9b620ccd866621c3
Anar <anar@rocky.anar.com>
2024-05-05 20:49:39 -0700

    added insertion

--- db_functions.c
+++ db_functions.c
@@ -10,7 +10,11 @@
 MYSQL *conn;
 // MYSQL_RES *res;
 // MYSQL_ROW row;
-
+ char *server = "192.168.1.110";
+ char *user = "root";
+ char *password = "password"; // Replace with your actual password
+ char *database = "expenses"; // Replace with your desired database name
+#define MAX_STRING 256
 void freemysqlrsource(MYSQL_RES *res, MYSQL *conn)
 {
     mysql_free_result(res);
@@ -20,10 +24,10 @@ void freemysqlrsource(MYSQL_RES *res, MYSQL *conn)
 MYSQL_RES *sendquery(char *query)
 {
     MYSQL_RES *res;
-    char *server = "192.168.1.110";
-    char *user = "root";
-    char *password = "password"; // Replace with your actual password
-    char *database = "expenses"; // Replace with your desired database name
+    //char *server = "192.168.1.110";
+    //char *user = "root";
+    //char *password = "password"; // Replace with your actual password
+    //char *database = "expenses"; // Replace with your desired database name
     conn = mysql_init(NULL);
     // Connect to the database
     if (!mysql_real_connect(conn, server, user, password, database, 0, NULL, 0))
@@ -41,3 +45,31 @@ MYSQL_RES *sendquery(char *query)
     res = mysql_use_result(conn);
     return res;
 }
+
+
+int add_gas_expense(char * datetime,double gallons,double price,char * address)
+{
+   //snprintf(query, MAX_STRING, "INSERT INTO users (id, name) VALUES (%d, '%s')", id, name);
+   //INSERT INTO `expenses`.`gas_business` (`purshase_date`, `gallons`, `price_g`, `store_addr`) VALUES ('2024-05-05 14:30:00.0000', '3', '4', 'test');
+   conn = mysql_init(NULL);
+     if (!mysql_real_connect(conn, server, user, password, database, 0, NULL, 0))
+  {
+      fprintf(stderr, "Error: %s\n", mysql_error(conn));
+      exit(1);
+  }
+   
+    char * query;
+     snprintf(query, MAX_STRING, "INSERT INTO `expenses`.`gas_business` (`purshase_date`, `gallons`, `price_g`, `store_addr`) VALUES ('%s', '%.2f', '%.2f', '%s');", datetime, gallons,price,address);
+ printf(query);
+ printf("\n");
+        if (mysql_query(conn, query) == 0) {
+            printf("Data inserted successfully!\n");
+            return 0;
+        } else {
+            printf("Error: %s\n", mysql_error(conn));
+            return 1;
+        }
+
+}
+
+
--- test.c
+++ test.c
@@ -14,6 +14,21 @@ int main()
         printf("%s\n", row[1]);
     }
 
-    freemysqlrsource(res, conn);
+   freemysqlrsource(res, conn);
+    add_gas_expense("2024-05-05",2.5,3.6,"ttest3");
+    add_gas_expense("2024-07-05",2,3,"ttesft3");
+    add_gas_expense("2024-06-05",2.7,3,"ttest3");
+    add_gas_expense("2024-08-05",2,3,"ff");
+    add_gas_expense("2024-06-05",2,3.9,"normalnm");
+    add_gas_expense("2024-09-05",2,3,"ttfbbest3");
+    add_gas_expense("2024-04-27",4.45,4.799,"1515 164 ave SW Lynnwood WA");
+    
+
+     res = sendquery("select sum(gallons *price_g) from expenses.gas_business");
+ while ((row = mysql_fetch_row(res)) != NULL)
+ {
+     printf("%s\n", row[0]);
+ }
+  freemysqlrsource(res, conn);
     return 0;
 }
