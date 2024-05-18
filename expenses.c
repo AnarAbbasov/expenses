afe95bf57a3ef274174176e5056137eef2a84757
Anar <anar@rocky.anar.com>
2024-05-18 07:32:36 -0700

    added menu navigation

--- expenses.c
+++ expenses.c
@@ -8,25 +8,62 @@ MYSQL_ROW row = NULL;
 
 MENU *menu;
 
-void show_entry_form(WINDOW * form_window)
+void show_entry_form(WINDOW *form_window)
 {
-    FIELD *fields[2];
-    fields[0] = new_field(1, 20, 2, 2, 0, 0);
-    fields[1] = new_field(2, 20, 2, 2, 0, 0);
-    fields[2] = NULL;
+    FIELD *fields[3];
+    fields[0] = new_field(1, 20, 3, 2, 0, 0); // Label purshase date
+    fields[1] = new_field(1, 20, 6, 2, 0, 0);
+    fields[2] = new_field(1, 20, 9, 2, 0, 0);
+    fields[3] = NULL;
     set_field_opts(fields[0], O_VISIBLE | O_PUBLIC | O_EDIT | O_ACTIVE);
-    set_field_back(fields[0], A_UNDERLINE);
-    set_field_back(fields[1], A_UNDERLINE);
+    set_field_back(fields[0], O_VISIBLE | O_PUBLIC | O_EDIT | O_ACTIVE);
+    set_field_back(fields[1], O_VISIBLE | O_PUBLIC | O_EDIT | O_ACTIVE);
+    set_field_back(fields[2], O_VISIBLE | O_PUBLIC | O_EDIT | O_ACTIVE);
     // Display the form
     FORM *my_form = new_form(fields);
-   
-   
-    set_form_sub(my_form, form_window);
-    //set_form_sub(my_form, derwin(form_window, 10, 50, 0, 0));
+
+    // set_form_sub(my_form, form_window);
+
+    set_form_sub(my_form, derwin(form_window, 0, 0, 1, 1));
+    wrefresh(form_window);
     post_form(my_form);
-     
-     wrefresh(form_window);
-     
+    mvwprintw(form_window, 3, 3, "DATE OF PURCHASE");
+    mvwprintw(form_window, 6, 3, "GALLONS");
+    mvwprintw(form_window, 9, 3, "PRICE/GALLON");
+    wrefresh(form_window);
+    keypad(form_window, TRUE);
+    char ch;
+    while ((ch = wgetch(form_window)) != KEY_F(1))
+    {
+        switch (ch)
+        {
+        case 82:
+
+            /* Go to next field */
+
+            form_driver(my_form, REQ_NEXT_FIELD);
+            /* Go to the end of the present buffer */
+            /* Leaves nicely at the last character */
+            form_driver(my_form, REQ_END_LINE);
+            break;
+        case 83:
+            /* Go to previous field */
+            form_driver(my_form, REQ_PREV_FIELD);
+            form_driver(my_form, REQ_END_LINE);
+            break;
+        case 10:
+            /* Go to previous field */
+            mvwprintw(form_window, 9, 13, "XUY");
+            wrefresh(form_window);
+           
+    break;
+        default:
+            /* If this is a normal character, it gets */
+            /* Printed				  */
+            form_driver(my_form, ch);
+            break;
+        }
+    }
 }
 
 void display_menu(WINDOW *main_window)
@@ -37,8 +74,7 @@ void display_menu(WINDOW *main_window)
     items[2] = NULL;
     menu = new_menu(items);
 
-   set_menu_sub(menu,derwin(main_window, 6, 38, 3, 100));
-
+    set_menu_sub(menu, derwin(main_window, 6, 38, 3, 100));
 
     post_menu(menu);
 
@@ -104,10 +140,13 @@ int main()
         case 'u':
             menu_driver(menu, REQ_UP_ITEM);
             break;
+
         case 10:
-           WINDOW * form_window=newwin(10, 30, 0, 0);
-           box(form_window,0,0);
-           show_entry_form(form_window);
+            WINDOW *form_window = newwin(40, 80, 2, 80);
+            box(form_window, 0, 0);
+
+            wbkgd(form_window, COLOR_PAIR(2));
+            show_entry_form(form_window);
             break;
         }
     }
